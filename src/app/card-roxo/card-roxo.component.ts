e86a3a5102c469deecc7e5c3a6f9896e7d30a1c0
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-04-09 14:56:45 -0300

    construct: criação de um component parte 2

---
+++ src/app/card-roxo/card-roxo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-roxo',
+  standalone: false,
+  templateUrl: './card-roxo.component.html',
+  styleUrl: './card-roxo.component.scss',
+})
+export class CardRoxoComponent {}
