3a991277524449d772c2f7f37b8aac168eea7baa
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-04-09 15:28:03 -0300

    Criação de componentes filhos

---
+++ src/app/card-button/card-button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-button',
+  standalone: false,
+  templateUrl: './card-button.component.html',
+  styleUrl: './card-button.component.scss',
+})
+export class CardButtonComponent {}
