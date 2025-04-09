4ae7bf7f925b3a3bb22b9fa9197c37c0cb763d52
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-04-09 17:20:00 -0300

    Template Dentro do Componente (prop "template")

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-card-button',
   standalone: false,
   templateUrl: './card-button.component.html',
+  // template: `<div class="card-button">Adquirir</div>`, Pode ser usado para testes
   styleUrl: './card-button.component.scss',
 })
 export class CardButtonComponent {}
