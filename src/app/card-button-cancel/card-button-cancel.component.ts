25da2f56da77974ed985c6900d671358f64aa200
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-04-09 18:41:09 -0300

    Utilização do Seletor "::ng-deep”

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -4,20 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-card-button-cancel',
   standalone: false,
   templateUrl: './card-button-cancel.component.html',
-  // styleUrl: './card-button-cancel.component.scss'
-  styles: [
-    `
-      .card-button-cancel {
-        font-size: 16px;
-        text-align: center;
-        margin-top: 10px;
-        border-radius: 25px;
-        background-color: red;
-        cursor: pointer;
-        font-weight: bold;
-        padding: 10px;
-      }
-    `,
-  ],
+  styleUrl: './card-button-cancel.component.scss',
 })
 export class CardButtonCancelComponent {}
